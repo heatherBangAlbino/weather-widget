9dc83ed5d66a855df8273c6cca297e6178529937
Heather Tompson <heathertompson@Heathers-MacBook-Air.local>
2022-08-23 18:40:54 -0400

    set-up fetch API

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("HELLURRRR")
+
+const api_url = "https://catfact.ninja/breeds";
+
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    
+}
+
+getapi(api_url);
